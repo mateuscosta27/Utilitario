4b5a476f790ecb6ea973068280f9ab0f7c7c8f90
mateuscosta27 <mateuscosta27@gmail.com>
2022-12-29 14:39:43 -0400

    Commit inicial

---
+++ configs/config.py
@@ -0,0 +1,60 @@
+import sqlite3
+import sys, os
+from PyQt5.QtWidgets import QApplication, QFileDialog, QMessageBox
+from sttle import *
+
+class Conf():
+
+
+    def __init__(self):
+        app = QApplication(sys.argv)
+        selected_directory = QFileDialog.getExistingDirectory(directory="C:/")
+        self.directory = (selected_directory+'/Utilitario/Banco')
+        if not os.path.exists(self.directory):
+            information = QMessageBox()
+            information.setIcon(QMessageBox.Information)
+            information.setText(f"Os arquivos serão salvos no destino {self.directory}")
+            information.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+            response = information.exec_()
+            if response == QMessageBox.Ok:
+                os.makedirs(self.directory)  
+                create = QMessageBox()
+                create.setText("Os diretórios foram criados com sucesso!")
+                create.exec_()
+                
+    def create_database(self):
+        db = sqlite3.connect(self.directory+'/utilitario.db')
+        cursor = db.cursor()
+        cursor.execute(
+            """"
+            CREATE TABLE IF NOT EXISTS tb_setor (
+                IdSetor INTEGER PRIMARY KEY,
+                nome varchar(50)
+                """)
+        db.commit()
+
+        cursor.execute(
+            """""
+            CREATE TABLE IF NOT EXISTS tb_funcionario (
+                IdFuncionario INTEGER PRIMARY KEY AUTOINCREMENT,
+                Funcionario varchar(50),
+                Setor varchar(50),
+                ramal_interno varchar(50),
+                ramal_externo varchar(50),
+                celular varchar(50),
+                Id_setor INTEGER foreign key REFERENCES tb_setor(IdSetor)
+            """
+        )
+        db.commit()
+            
+
+teste = Conf()
+teste.create_database()
+
+
+
+
+
+
+
+    
---
+++ configs/sttle.py
@@ -0,0 +1,11 @@
+class Style:
+    def __init__(self):
+        pass
+    def button(self):
+        "QPushButton{border-style: none; border-width: 2px; font: 75 14pt \"MS Shell Dlg 2\";\n"
+"    color: rgb(255, 255, 255);\n"
+"    background-color: #0954B2;\n"
+"    border-radius: 15px;\n"
+"    border-bottom-color: rgb(4, 35, 38);\n"
+"    border-right-color:  rgb(4, 35, 38);\n"
+"}"
